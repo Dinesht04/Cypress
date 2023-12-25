db8ee7fe2d408320e327d719adf377c4a7aa7534
Dinesh Tyagi <dineshtyagi567@gmail.com>
2023-12-26 00:48:29 +0530

    Setup the Drizzle-ORM and Supabase DB and Schema,now beginning with ShadCN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import db from '@/lib/supabase/db';
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +17,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+    console.log(db);
   return (
+
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
